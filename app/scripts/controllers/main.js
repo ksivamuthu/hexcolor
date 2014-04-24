75163eaeb2fd57f7050a97b49a99264c7415d580
Siva <ksivamuthu@ceiamerica.com>
2014-04-24 13:53:18 -0400

    Hex color site

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,4 @@
 'use strict';
-
-angular.module('hexcolorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('hexcolorApp').controller('MainCtrl', function ($scope) {
+	$scope.hexcolor = '#fff';
+});
